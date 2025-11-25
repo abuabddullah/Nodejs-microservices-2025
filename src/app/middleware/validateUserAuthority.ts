3cd7d10df7b6b775ea9d3d4049a4924b871bed8c
asifaowadud <asifaowadud@gmail.com>
2025-11-25 08:55:25 +0600

    feat: Implement `validateUserAuthority` middleware for user validation via `x-user-id` header and remove local Docker Compose setup.

---
+++ src/app/middleware/validateUserAuthority.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../errors/AppError';
+
+const validateUserAuthority =
+     (...roles: string[]) =>
+     async (req: Request, res: Response, next: NextFunction) => {
+          try {
+               //    const tokenWithBearer = req.headers.authorization;
+               const stringyfiedUser = req.headers['x-user-id'] as string;
+               if (!stringyfiedUser) {
+                    throw new AppError(StatusCodes.UNAUTHORIZED, 'You are not authorized !!');
+               }
+               const user = JSON.parse(stringyfiedUser);
+               if (!user) {
+                    throw new AppError(StatusCodes.NOT_FOUND, 'This user is not found !!');
+               }
+
+               if (user?.status === 'blocked') {
+                    throw new AppError(StatusCodes.FORBIDDEN, 'This user is blocked !!');
+               }
+
+               if (user?.isDeleted) {
+                    throw new AppError(StatusCodes.FORBIDDEN, 'This user accaunt is deleted !!');
+               }
+
+               //guard user
+               if (roles.length && !roles.includes(user?.role)) {
+                    throw new AppError(StatusCodes.FORBIDDEN, "You don't have permission to access this api !*-!");
+               }
+
+               //set user to header
+               req.user = user;
+               next();
+          } catch (error) {
+               next(error);
+          }
+     };
+
+export default validateUserAuthority;
--- src/app/modules/post/post.route.ts
+++ src/app/modules/post/post.route.ts
@@ -1,23 +1,23 @@
 import express from 'express';
 import { USER_ROLES } from '../../../enums/user';
-import auth from '../../middleware/auth';
+import validateUserAuthority from '../../middleware/validateUserAuthority';
 import validateRequest from '../../middleware/validateRequest';
 import { postController } from './post.controller';
 import { postValidation } from './post.validation';
 
 const router = express.Router();
 
-router.post('/', auth(USER_ROLES.USER), validateRequest(postValidation.createPostZodSchema), postController.createPost);
+router.post('/', validateUserAuthority(USER_ROLES.USER), validateRequest(postValidation.createPostZodSchema), postController.createPost);
 
 router.get('/', postController.getAllPosts);
 
 router.get('/unpaginated', postController.getAllUnpaginatedPosts);
 
-router.delete('/hard-delete/:id', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), postController.hardDeletePost);
+router.delete('/hard-delete/:id', validateUserAuthority(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), postController.hardDeletePost);
 
-router.patch('/:id', auth(USER_ROLES.USER), validateRequest(postValidation.updatePostZodSchema), postController.updatePost);
+router.patch('/:id', validateUserAuthority(USER_ROLES.USER), validateRequest(postValidation.updatePostZodSchema), postController.updatePost);
 
-router.delete('/:id', auth(USER_ROLES.USER), postController.deletePost);
+router.delete('/:id', validateUserAuthority(USER_ROLES.USER), postController.deletePost);
 
 router.get('/:id', postController.getPostById);
 
